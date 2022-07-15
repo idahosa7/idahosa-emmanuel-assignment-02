dd477d6bdc5834aed9402d8331b49bea75138695
idahosa <105437059+idahosa7@users.noreply.github.com>
2022-07-15 20:20:42 +0000

    Finished the assignment

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let sum = 0;
+  
+  for (i = 0; i < arrayOfNumbers.length; i++){
+    sum += arrayOfNumbers[i]
+  }
+return sum  
+
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenNumber = arrayOfNumbers.filter(num => {
+    return num % 2 === 0
+ });
+ 
+  
+    return evenNumber.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
